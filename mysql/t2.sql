f2074c5cae3b1e20b88ec840ce3d8d67ba6afa24
smiledip <smile_dip@163.com>
2018-04-14 17:43:17 +0800

    mysql sql

---
+++ mysql/t2.sql
@@ -0,0 +1,30 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50157
+Source Host           : localhost:3306
+Source Database       : bddj
+
+Target Server Type    : MYSQL
+Target Server Version : 50157
+File Encoding         : 65001
+
+Date: 2018-04-14 17:40:01
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for t2
+-- ----------------------------
+DROP TABLE IF EXISTS `t2`;
+CREATE TABLE `t2` (
+  `name1` varchar(255) DEFAULT NULL,
+  `name2` varchar(255) DEFAULT NULL,
+  `name3` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t2
+-- ----------------------------
